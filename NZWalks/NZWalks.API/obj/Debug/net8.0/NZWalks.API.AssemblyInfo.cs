06ab347eca731218e6e88b970e8238dff4209300
Nafiz Imtiaz Khan <nafiz.ba.pakizatvl@gmail.com>
2025-01-06 20:51:30 +0600

    Random

--- NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+715469be1435cbd2047b9b53cd87233f07b0f519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133aa0623b174899898a09a4e5cd16c85a791b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
